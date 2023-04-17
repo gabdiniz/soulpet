ab1b86d3009535d096eef56b0effbfb9714dc5f8
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-17 16:42:59 -0300

    Listagens de todos clientes e detalhe

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ authenticate(connection); // efetivar conexão
 const Cliente = require("./database/cliente"); // configura model da aplicação
 const Endereco = require("./database/endereco");
 
+app.get("/clientes", async (req, res) => {
+  const listaClientes = await Cliente.findAll();
+  res.json(listaClientes);
+})
+
+app.get("/clientes/:id", async (req, res) => {
+  const cliente = await Cliente.findOne({
+    where: { id: req.params.id },
+    include: [Endereco]    
+  });
+  (cliente) ? res.json(cliente) : res.status(404).json({message: "Cliente não encontrado"})
+})
+
 //Definição de rotas
 app.post("/clientes", async (req, res) => {
   const { nome, email, telefone, endereco } = req.body; // Coletar informação do req.body
@@ -20,7 +33,7 @@ app.post("/clientes", async (req, res) => {
     const novo = await Cliente.create(  // chamar o Model.create
       { nome, email, telefone, endereco },
       { include: [Endereco] } // permite inserir o endereço junto do cliente
-    ); 
+    );
     res.status(201).json(novo);
   }
   catch (e) {
